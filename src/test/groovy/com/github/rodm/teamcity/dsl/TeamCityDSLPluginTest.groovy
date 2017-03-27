0f1cbd6e5e07b026baf0a10a578293f0d7be1e4a
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-03-27 15:12:27 +0100

    Setup build for running tests and add first test

---
+++ src/test/groovy/com/github/rodm/teamcity/dsl/TeamCityDSLPluginTest.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 Rod MacKenzie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rodm.teamcity.dsl
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+
+import static org.junit.Assert.assertTrue
+
+class TeamCityDSLPluginTest {
+
+    @Rule
+    public final TemporaryFolder projectDir = new TemporaryFolder()
+
+    private Project project
+
+    @Before
+    void setup() {
+        project = ProjectBuilder.builder().withProjectDir(projectDir.root).build()
+    }
+
+    @Test
+    void 'applying plugin adds teamcityConfig extension'() {
+        project.apply plugin: 'com.github.rodm.teamcity-dsl'
+
+        assertTrue(project.extensions.getByName('teamcityConfig') instanceof TeamCityDSLExtension)
+    }
+}
