0705d8789066f8cd0f6eeab1993a33d81b5245b2
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-03-27 15:52:45 +0100

    Add tests for task configuration

--- src/test/groovy/com/github/rodm/teamcity/dsl/TeamCityDSLPluginTest.groovy
+++ src/test/groovy/com/github/rodm/teamcity/dsl/TeamCityDSLPluginTest.groovy
@@ -23,6 +23,10 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
 
+import static org.hamcrest.Matchers.endsWith
+import static org.hamcrest.Matchers.is
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertThat
 import static org.junit.Assert.assertTrue
 
 class TeamCityDSLPluginTest {
@@ -42,5 +46,48 @@ class TeamCityDSLPluginTest {
         project.apply plugin: 'com.github.rodm.teamcity-dsl'
 
         assertTrue(project.extensions.getByName('teamcityConfig') instanceof TeamCityDSLExtension)
+    }
+
+    @Test
+    void 'applying plugin adds teamcity configuration'() {
+        project.apply plugin: 'com.github.rodm.teamcity-dsl'
+
+        def configuration = project.configurations.findByName('teamcity')
+        assertNotNull(configuration)
+    }
+
+    @Test
+    void 'applying plugin adds generateConfiguration task'() {
+        project.apply plugin: 'com.github.rodm.teamcity-dsl'
+
+        def task = project.tasks.findByName('generateConfiguration')
+        assertNotNull(task)
+    }
+
+    @Test
+    void 'generateConfiguration task is configured with default values'() {
+        project.apply plugin: 'com.github.rodm.teamcity-dsl'
+
+        GenerateConfigurationTask task = project.tasks.findByName('generateConfiguration') as GenerateConfigurationTask
+        assertThat(task.format, is('kotlin'))
+        assertThat(normalizePath(task.baseDir), endsWith('/.teamcity'))
+        assertThat(normalizePath(task.destDir), endsWith('/build/generated-configs'))
+    }
+
+    @Test
+    void 'generateConfiguration task is configured with alternative values'() {
+        project.apply plugin: 'com.github.rodm.teamcity-dsl'
+        project.teamcityConfig {
+            baseDir = project.file('src/test/teamcity')
+            destDir = project.file('data/10.0/config/projects')
+        }
+
+        GenerateConfigurationTask task = project.tasks.findByName('generateConfiguration') as GenerateConfigurationTask
+        assertThat(normalizePath(task.baseDir), endsWith('/src/test/teamcity'))
+        assertThat(normalizePath(task.destDir), endsWith('/data/10.0/config/projects'))
+    }
+
+    private static String normalizePath(File path) {
+        path.canonicalPath.replace('\\', '/')
     }
 }
