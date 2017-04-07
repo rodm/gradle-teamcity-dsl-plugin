022cfa7e2cf65d6b29144e8466e3281a64a7c4a0
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2017-04-07 09:29:55 +0100

    Clean up code

--- src/functional/groovy/com/github/rodm/teamcity/dsl/TeamCityDSLPluginFunctionalTest.groovy
+++ src/functional/groovy/com/github/rodm/teamcity/dsl/TeamCityDSLPluginFunctionalTest.groovy
@@ -70,14 +70,21 @@ class TeamCityDSLPluginFunctionalTest {
         }
     '''.stripIndent()
 
+    static final String PROJECT_CONFIG_PATH = 'build/generated-configs/Project/project-config.xml'
+
+    static final String DSL_EXCEPTION_PATH = 'build/generated-configs/dsl_exception.xml'
+
     @Rule
     public final TemporaryFolder testProjectDir = new TemporaryFolder()
 
     private File buildFile
 
+    private List<String> arguments
+
     @Before
     void setup() throws IOException {
         buildFile = testProjectDir.newFile("build.gradle")
+        arguments = ['-S', 'generateConfiguration', '-Pserver=' + System.properties['teamcity.server.host']]
     }
 
     @Test
@@ -91,14 +98,14 @@ class TeamCityDSLPluginFunctionalTest {
         BuildResult result = GradleRunner.create()
                 .forwardOutput()
                 .withProjectDir(testProjectDir.getRoot())
-                .withArguments(['-S', 'generateConfiguration', '-Pserver=' + System.properties['teamcity.server.host']])
+                .withArguments(arguments)
                 .withPluginClasspath()
                 .build()
 
         assertEquals(result.task(":generateConfiguration").getOutcome(), SUCCESS)
-        File projectFile = new File(testProjectDir.root, 'build/generated-configs/Project/project-config.xml')
+        File projectFile = new File(testProjectDir.root, PROJECT_CONFIG_PATH)
         assertTrue(projectFile.exists())
-        File exceptionFile = new File(testProjectDir.root, 'build/generated-configs/dsl_exception.xml')
+        File exceptionFile = new File(testProjectDir.root, DSL_EXCEPTION_PATH)
         assertFalse(exceptionFile.exists())
     }
 
@@ -113,14 +120,14 @@ class TeamCityDSLPluginFunctionalTest {
         BuildResult result = GradleRunner.create()
                 .forwardOutput()
                 .withProjectDir(testProjectDir.getRoot())
-                .withArguments(['-S', 'generateConfiguration', '-Pserver=' + System.properties['teamcity.server.host']])
+                .withArguments(arguments)
                 .withPluginClasspath()
                 .buildAndFail()
 
         assertEquals(FAILED, result.task(":generateConfiguration").getOutcome())
-        File projectFile = new File(testProjectDir.root, 'build/generated-configs/Project/project-config.xml')
+        File projectFile = new File(testProjectDir.root, PROJECT_CONFIG_PATH)
         assertFalse(projectFile.exists())
-        File exceptionFile = new File(testProjectDir.root, 'build/generated-configs/dsl_exception.xml')
+        File exceptionFile = new File(testProjectDir.root, DSL_EXCEPTION_PATH)
         assertTrue(exceptionFile.exists())
     }
 
@@ -135,11 +142,11 @@ class TeamCityDSLPluginFunctionalTest {
         GradleRunner.create()
                 .forwardOutput()
                 .withProjectDir(testProjectDir.getRoot())
-                .withArguments(['-S', 'generateConfiguration', '-Pserver=' + System.properties['teamcity.server.host']])
+                .withArguments(arguments)
                 .withPluginClasspath()
                 .buildAndFail()
 
-        File exceptionFile = new File(testProjectDir.root, 'build/generated-configs/dsl_exception.xml')
+        File exceptionFile = new File(testProjectDir.root, DSL_EXCEPTION_PATH)
         assertTrue(exceptionFile.exists())
 
         settingsFile.text = VALID_SETTINGS_FILE
@@ -147,7 +154,7 @@ class TeamCityDSLPluginFunctionalTest {
         GradleRunner.create()
                 .forwardOutput()
                 .withProjectDir(testProjectDir.getRoot())
-                .withArguments(['-S', 'generateConfiguration', '-Pserver=' + System.properties['teamcity.server.host']])
+                .withArguments(arguments)
                 .withPluginClasspath()
                 .build()
 
